eeef67eec811f8f19a9cbc2b2e5a583fa6cd65c0
Everton Paixao <everton.paixao@somagrupo.com.br>
2025-03-19 15:41:34 -0300

    feat: adjust events and messaging system

--- apps/api/src/shared/event/project/project.events.ts
+++ apps/api/src/shared/event/project/project.events.ts
@@ -3,3 +3,8 @@ export enum ProjectEvent {
   UPDATED = 'project.updated',
   DELETED = 'project.deleted',
 }
+
+export enum InviteEvent {
+  CREATED_INVITATION = 'project.created.invitation',
+  CONFIRM_INVITATION =  'project.invitation.confirmed'
+}
